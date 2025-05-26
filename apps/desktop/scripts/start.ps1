232c4076f4b105e4cb3155f48a9dae8489678bf2
IgnacioBona <ignacio.bona-ribot@epitech.eu>
2025-05-26 15:08:26 +0800

    [feat] Add global desktop start script

---
+++ apps/desktop/scripts/start.ps1
@@ -0,0 +1,2 @@
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd ./frontend; ./scripts/start_dev.ps1"
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd ./backend; ./scripts/run.ps1"
