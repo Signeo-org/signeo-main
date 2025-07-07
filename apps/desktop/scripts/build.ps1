88dd115e1d59c928541f1382153951ff0b1f57ff
IgnacioBona <ignacio.bona-ribot@epitech.eu>
2025-07-07 04:57:36 +0200

    [ADD] Improved build script

--- apps/desktop/scripts/build.ps1
+++ apps/desktop/scripts/build.ps1
@@ -1,14 +1,21 @@
 
-# Use the build script from the backend
+# Build the backend
 Set-Location -Path "./backend"
 ./scripts/build.ps1
 
-# Copy the backend build to the frontend
+# Copy the backend build to the frontend resources
 Set-Location -Path "../"
-Copy-Item -Path "backend/build/Release/*.exe" -Destination "frontend/resources" -Recurse -Force
-Copy-Item -Path "backend/build/Release/models/*.bin" -Destination "frontend/resources/models" -Recurse -Force
+if (Test-Path "backend/build/Release/*.exe") {
+    New-Item -ItemType Directory -Path "frontend/resources" -Force | Out-Null
+    Copy-Item -Path "backend/build/Release/*.exe" -Destination "frontend/resources/" -Recurse -Force
+}
 
-# Use the build script from the frontend
+if (Test-Path "backend/build/Release/models/*.bin") {
+    New-Item -ItemType Directory -Path "frontend/resources/models" -Force | Out-Null
+    Copy-Item -Path "backend/build/Release/models/*.bin" -Destination "frontend/resources/models/" -Recurse -Force
+}
+
+# Build the frontend
 Set-Location -Path "./frontend"
 ./scripts/build.ps1
 Set-Location -Path "../"
